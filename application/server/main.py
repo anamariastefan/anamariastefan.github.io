f61c97bcf1931410fd6a181b913b62675c5eaa65
anamariastefan <67341798+anamariastefan@users.noreply.github.com>
2021-12-28 10:43:30 +0200

    Add files via upload

---
+++ application/server/main.py
@@ -0,0 +1,31 @@
+import uvicorn
+from fastapi import FastAPI, File, UploadFile
+from starlette.responses import RedirectResponse
+
+from application.components import predict, read_imagefile
+from application.schema import Symptom
+from application.components.prediction import symptom_check
+
+app_desc = """<h2>Upload an image with `predict/image`</h2>
+
+
+app = FastAPI(title='Tensorflow FastAPI Starter Pack', description=app_desc)
+
+
+@app.get("/", include_in_schema=False)
+async def index():
+    return RedirectResponse(url="/docs")
+
+
+@app.post("/predict/image")
+async def predict_api(file: UploadFile = File(...)):
+    extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
+    if not extension:
+        return "Image must be jpg or png format!"
+    image = read_imagefile(await file.read())
+    prediction = predict(image)
+
+    return prediction
+
+if __name__ == "__main__":
+    uvicorn.run(app, debug=True)
